fe856cf564c1941a1decc2a33673618c8d7cccaa
Nep T. Orshiso <nep.orshiso@gmail.com>
2021-08-04 20:40:20 -0400

    built out the recursive call to grab all results from api using .pipe/.expand from rxjs. grabbing axios for client.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { EMPTY, expand, reduce } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private authToken: string;
+
+  constructor(
+    private configService: ConfigService,
+    private httpService: HttpService,
+  ) {
+    this.authToken = this.configService.get('API_KEY');
+  }
+
+  async getAllPeople(pageNum = 1) {
+    return this.httpService
+      .get(`https://api.salesloft.com/v2/people.json?page=${pageNum}`, {
+        headers: { Authorization: `Bearer ${this.authToken}` },
+      })
+      .pipe(
+        expand((res: any) =>
+          res.data.metadata.paging.next_page
+            ? this.httpService.get(
+                `https://api.salesloft.com/v2/people.json?page=${res.data.metadata.paging.next_page}`,
+                {
+                  headers: { Authorization: `Bearer ${this.authToken}` },
+                },
+              )
+            : EMPTY,
+        ),
+        reduce((acc, res) => acc.concat(res.data), []),
+      );
   }
 }
