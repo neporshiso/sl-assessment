5c9ccbc8a0aa10c5e3673e81ef819eacde9c4bfe
Nep T. Orshiso <nep.orshiso@gmail.com>
2021-08-05 12:53:10 -0400

    level 3 logic done

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [showTable, setShowTable] = React.useState(1);
   const [peopleData, setPeopleData] = React.useState([]);
   const [charCountData, setCharCountData] = React.useState([]);
+  const [duplicatePeopleData, setDuplicatePeopleData] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
@@ -48,6 +49,57 @@ function App() {
     setShowTable(2);
   };
 
+  const handleLevelThreeClick = () => {
+    const potentialDuplicates = [];
+    const emailAddressArr = peopleData.map((p) => p.email_address);
+    // Business Logic to determine if two emails potentially belong to the same person
+    const substringCheck = (currentEmail, evaluatingEmail) => {
+      // fail fast if you're comparing the same email
+      if (currentEmail === evaluatingEmail) return false;
+
+      const currentEmailDomain = currentEmail.substring(
+        currentEmail.indexOf('@') + 1,
+      );
+
+      const evaluatingEmailDomain = evaluatingEmail.substring(
+        evaluatingEmail.indexOf('@') + 1,
+      );
+
+      // Want to detect different domains and return false early to prevent false positive detections
+      if (currentEmailDomain !== evaluatingEmailDomain) return false;
+
+      const currentEmailPreAt = currentEmail.substring(
+        0,
+        currentEmail.indexOf('@'),
+      );
+      const evaluatingEmailPreAt = evaluatingEmail.substring(
+        0,
+        currentEmail.indexOf('@'),
+      );
+
+      if (evaluatingEmailPreAt.indexOf(currentEmailPreAt) > -1) return true;
+      return false;
+    };
+
+    for (let i = 0; i < emailAddressArr.length; i++) {
+      for (let j = 0; j < emailAddressArr.length; j++) {
+        if (substringCheck(emailAddressArr[i], emailAddressArr[j])) {
+          potentialDuplicates.push(emailAddressArr[i], emailAddressArr[j]);
+        }
+      }
+    }
+
+    const duplicatedPersons = [];
+    potentialDuplicates.forEach((email) => {
+      const person = peopleData.find((el) => el.email_address === email);
+      duplicatedPersons.push(person);
+    });
+
+    setShowTable(3);
+    setDuplicatePeopleData(duplicatedPersons);
+    return;
+  };
+
   return (
     <div
       style={{
@@ -59,27 +111,57 @@ function App() {
       }}
     >
       {showTable === 1 && (
-        <TableOne isLoading={isLoading} rows={peopleData} pageSize={25} />
+        <>
+          <TableOne isLoading={isLoading} rows={peopleData} pageSize={25} />
+          <div
+            style={{
+              justifySelf: 'flex-start',
+              alignSelf: 'center',
+              minWidth: '900px',
+              marginTop: '10px',
+            }}
+          >
+            {/* Disable button if isloaidng */}
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleLevelTwoClick()}
+            >
+              Show Character Frequency Count
+            </Button>
+          </div>
+        </>
       )}
       {showTable === 2 && (
-        <TableTwo isLoading={isLoading} rows={charCountData} pageSize={25} />
+        <>
+          <TableTwo isLoading={isLoading} rows={charCountData} pageSize={25} />
+          <div
+            style={{
+              justifySelf: 'flex-start',
+              alignSelf: 'center',
+              minWidth: '400px',
+              marginTop: '10px',
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => handleLevelThreeClick()}
+            >
+              Show Potential Duplicates
+            </Button>
+          </div>
+        </>
       )}
-      <div
-        style={{
-          justifySelf: 'flex-start',
-          alignSelf: 'center',
-          minWidth: '900px',
-          marginTop: '10px',
-        }}
-      >
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => handleLevelTwoClick()}
-        >
-          Show Character Frequency Count
-        </Button>
-      </div>
+      {showTable === 3 && (
+        <>
+          <TableOne
+            isLoading={isLoading}
+            rows={duplicatePeopleData}
+            pageSize={25}
+          />
+        </>
+      )}
     </div>
   );
 }
